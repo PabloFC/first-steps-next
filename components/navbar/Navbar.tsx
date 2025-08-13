021ebdd11c30a998194695f1362bb5ee9de3f5e7
PabloFC <pablofc_68@hotmail.com>
2025-08-13 20:12:26 +0200

    navbar fixed

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -13,17 +13,19 @@ const Navbar = () => {
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
 
       <Link href={'/'} className='flex items-center '>
-      <HomeIcon className='mr-2'/>
-      <span>Home</span>
+        <HomeIcon className='mr-2'/>
+        <span>Home</span>
       </Link>
 
-      <div className='flex flex-1'></div>
-
+      <div className='ml-auto flex gap-3'>
       {
         navItems.map(navItem => (
           <ActiveLink key={navItem.name} path={navItem.href} text={navItem.name} />
         ))
       }
+
+      </div>
+
 
   
     </nav>
